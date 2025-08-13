c1779b0dbf4d7c9c0f4b39d59f7c842db55a8e70
amyheather <a.heather2@exeter.ac.uk>
2025-08-13 16:31:40 +0100

    fix(test_equivalence): normalising output worked on linux and mac (e.g. /tmp/tmpabc123.py:) but not on windows as that has two colons (e.g. C:\Users\runneradmin\AppData\Local\Temp\tmprh56bprw.py:)

--- tests/test_equivalence.py
+++ tests/test_equivalence.py
@@ -53,7 +53,7 @@ def test_radon_hal_preserve_line_effect():
 
     def normalise_radon_output(output: str) -> str:
         """Remove strings from radon output, keeping numeric results."""
-        return re.sub(r'^[^:]+:', "<FILE>:", output, flags=re.MULTILINE)
+        return re.sub(r'^.*:', '<FILE>:', output, flags=re.MULTILINE)
 
     result_false = normalise_radon_output(run_radon_hal(False))
     result_true = normalise_radon_output(run_radon_hal(True))
